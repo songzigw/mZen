916549c21ce2bab23595e64474de1e1c9329012c
songzigw <songzigw@163.com>
2016-04-08 12:21:06 +0800

    ui

---
+++ src/script/nextalk-webui.js
@@ -0,0 +1,21 @@
+/*!
+ * nextalk-webui.js v0.0.1
+ * http://nextalk.im/
+ *
+ * Copyright (c) 2014 NexTalk
+ *
+ */
+
+var NexTalkWebUI = function() {
+};
+
+(function(WebUI, IM, undefined) {
+
+    "use strict";
+    
+    var top = window.top;
+    if (top != window.self) {
+        
+    }
+
+})(NexTalkWebUI, NexTalkWebIM);
