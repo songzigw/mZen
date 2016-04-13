4b7bcbe8a6bce1e9e82071cdf31be298b463c9ec
songzigw <songzigw@163.com>
2016-04-13 11:43:52 +0800

    show

--- src/script/nextalk-webui.js
+++ src/script/nextalk-webui.js
@@ -198,7 +198,10 @@ var NexTalkWebUI = function() {
         var _this = this;
         _this._uid = uid;
         window.setTimeout(function() {
-            _this._connectServer(uid);
+            //_this._connectServer(uid);
+            _this.els.$initPage.hide();
+            _this.onLogin();
+            _this.onLoginWin();
         }, 3000);
     };
     
