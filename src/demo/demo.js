52e40e5d911d4cd7178620d901d466339ca22ea9
songzigw <songzigw@163.com>
2016-04-25 19:39:37 +0800

    demo.js

---
+++ src/demo/demo.js
@@ -0,0 +1,29 @@
+var demo = {
+    resPath : '/nextalk-webim-song/src/',
+    apiPath : '/nextalk-webim-song/',
+    route : {
+        online : "online.do",
+        offline : "offline.do",
+        buddies : "buddies.do",
+        remove_buddy : "remove_buddy.do",
+        deactivate : "refresh.do",
+        message : "message.do",
+        presence : "presence.do",
+        status : "status.do",
+        setting : "setting.do",
+        history : "history.do",
+        clear : "clear_history.do",
+        download : "download_history.do",
+        // room actions
+        invite : "invite.do",
+        join : "join.do",
+        leave : "leave.do",
+        block : "block.do",
+        unblock : "unblock.do",
+        members : "members.do",
+        // notifications
+        notifications : "notifications.do",
+        // upload files
+        upload : "upload.do"
+    }
+};
